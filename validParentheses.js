f3dc7b1d7739edb250781eb6bad78289f8ecc3a3
Kun Chen <justinchen9387@gmail.com>
2022-06-01 18:10:34 -0700

    modified valid parentheses

--- validParentheses.js
+++ validParentheses.js
@@ -1,5 +1,8 @@
 /*
-Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
 
 An input string is valid if:
 
@@ -22,60 +25,47 @@ Output: false
 */
 
 /*
-I: a string contains '(', ')', '{', '}', '[' and ']'
-O: boolean value
+I: string
+O: boolean
 C:
-E: empty string
+E:
 */
 
-
-// if string is empty
-  // return false
-
-//convert string into array
-//while array length is not equal to lastLength
-  //set lastLength equal to array length
-  //iterate array
-    //if current element is '(' and next element is ')'
-    //delete current and next element from array
-    //if current element is '{' and next element is '}'
-    //delete current and next element from array
-    //if current element is '[' and next element is ']'
-    //delete current and next element from array
-
-//if array length is equal to 0
-  //return true
-// else
-  // return false
-
+// create a hashmap contains [] {}, and ()
+// create an empty array
+// iterate the string
+  // if current element is not in hashmap
+    // push current element into array
+  // else
+    // find the corresponding opening Parentheses
+    // if it is match to the last element of array
+      // remove last element from array
+    // else
+      // return false
+  // return true
   var isValid = function(s) {
-    if (s === '') {
-      return false;
+    let parenthesesMap = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
     }
+    let arr = [];
 
-    let lastLength = 0;
-    let sArray = s.split('');
-    while(sArray.length !== lastLength) {
-      lastLength = sArray.length;
-      for (let i = 0; i < sArray.length; i++) {
-        if(sArray[i] === '(' && sArray[i+1] ===')') {
-          sArray.splice(i, 2);
+    for (let i = 0; i < s.length; i++) {
+        if (parenthesesMap[s[i]] === undefined) {
+            arr.push(s[i]);
+        } else {
+            if (parenthesesMap[s[i]] === arr[arr.length - 1]) {
+                arr.pop();
+            } else {
+                return false;
+            }
         }
-        if(sArray[i] === '{' && sArray[i+1] ==='}') {
-          sArray.splice(i, 2);
-        }
-        if(sArray[i] === '[' && sArray[i+1] ===']') {
-          sArray.splice(i, 2);
-        }
-      }
     }
-    if(sArray.length === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+    return true;
+};
 
-  console.log(isValid("()[]{}"))
-  console.log(isValid("([]){}"))
-  console.log(isValid("(){]"))
+console.log(isValid("()[]{}")) //true
+console.log(isValid("({[]})")) //true
+console.log(isValid("(}")) //false
+console.log(isValid("{(})")) //false
